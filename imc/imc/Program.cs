6ed44f0a87dfc7d247dfb169773e02c7da1f5694
Daniel <danielprado28@gmail.com>
2021-03-17 10:52:24 -0300

    primeiro comit

---
+++ imc/imc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Properties.Pessoa(37, 78, "Bob Nelson", 1.70);
+        }
+    }
+}
---
+++ imc/imc/Properties/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imc.Properties
+{
+    class Pessoa
+    {
+        int idade;
+        float peso;
+        string nome;
+        double altura;
+
+        public Pessoa(int idade, float peso, string nome, double altura)
+        {
+            this.idade = idade;
+            this.peso = peso;
+            this.nome = nome;
+            this.altura = altura;
+        }
+    }
+}
